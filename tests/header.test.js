1da0ddef7a8a6d4b29868b016cba8a56890ec208
Leon <leon.ben2892@gmail.com>
2021-09-15 00:12:36 +0300

    added fake authentication for automated tests usage

--- tests/header.test.js
+++ tests/header.test.js
@@ -15,8 +15,44 @@ afterEach(async () => {
     await browser.close();
 });
 
-test('We can launch a browser', async () => {
+test('the header has the correct text', async () => {
     const text = await page.$eval('a.brand-logo', el => el.innerHTML);
 
     expect(text).toEqual('Blogster');
+});
+
+test('clicking login starts oauth flow', async () => {
+    await page.click('.right a');
+
+    const url = await page.url();
+
+    expect(url).toMatch(/accounts\.google\.com/);
+});
+
+test('When signed in, shows logout button', async () => {
+    const userId = '613b03158422552c544caf55';
+
+    const Buffer = require('safe-buffer').Buffer;
+    const sessionObject = {
+        passport: {
+            user: userId
+        }
+    };
+
+    const sessionString = Buffer.from(JSON.stringify(sessionObject)).toString('base64');
+
+    const Keygrip = require('keygrip');
+    const keys = require('../config/keys');
+
+    const keygrip = new Keygrip([keys.cookieKey]);
+    const sig = keygrip.sign('session=' + sessionString);
+    
+    await page.setCookie({ name: 'session', value: sessionString }) ;
+    await page.setCookie({ name: 'session.sig', value: sig });
+    await page.goto('localhost:3000');
+
+    await page.waitFor('a[href="/auth/logout"]');
+    const text = await page.$eval('a[href="/auth/logout"]', el => el.innerHTML);
+
+    expect(text).toEqual('Logout');
 });
