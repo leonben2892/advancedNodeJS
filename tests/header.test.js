ec414dc10e1b276960ad582a44cb70d518916b7d
Leon <leon.ben2892@gmail.com>
2021-09-15 16:26:05 +0300

    added factory functions to be used in tests

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,4 +1,6 @@
 const puppeteer = require('puppeteer');
+const sessionFactory = require('./factories/sessionFactory');
+const userFactory = require('./factories/userFactory');
 
 let browser, page;
 
@@ -29,25 +31,11 @@ test('clicking login starts oauth flow', async () => {
     expect(url).toMatch(/accounts\.google\.com/);
 });
 
-test('When signed in, shows logout button', async () => {
-    const userId = '613b03158422552c544caf55';
-
-    const Buffer = require('safe-buffer').Buffer;
-    const sessionObject = {
-        passport: {
-            user: userId
-        }
-    };
-
-    const sessionString = Buffer.from(JSON.stringify(sessionObject)).toString('base64');
-
-    const Keygrip = require('keygrip');
-    const keys = require('../config/keys');
-
-    const keygrip = new Keygrip([keys.cookieKey]);
-    const sig = keygrip.sign('session=' + sessionString);
+test.only('When signed in, shows logout button', async () => {
+    const user = await userFactory();
+    const { session, sig } = sessionFactory(user);
     
-    await page.setCookie({ name: 'session', value: sessionString }) ;
+    await page.setCookie({ name: 'session', value: session }) ;
     await page.setCookie({ name: 'session.sig', value: sig });
     await page.goto('localhost:3000');
 
---
+++ tests/setup.js
@@ -0,0 +1,8 @@
+require('../models/User');
+const mongoose = require('mongoose');
+const keys = require('../config/keys');
+
+Number.prototype._called = {};
+
+mongoose.Promise = global.Promise;
+mongoose.connect(keys.mongoURI, { useMongoClient: true });
